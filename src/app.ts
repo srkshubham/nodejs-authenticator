109f748f3fc3c3f7b378e3d6e8cf579785d81695
srkshubham <shubhamkumawat149@gmail.com>
2026-01-29 09:37:35 +0530

    production changes

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,30 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes";
 import userRouter from "./routes/user.routes";
 import adminRouter from "./routes/admin.routes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet);
 
-app.get("/heath", (_req, res) => {
-  return res.json({ statusbar: "ok" });
+app.get("/health", (_req, res) => {
+  res.status(200).send("OK");
 });
 
+app.get("/", (_req, res) => {
+  res.send("Backend is live 🚀");
+});
+
+app.use(
+  "/auth",
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+  })
+);
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
