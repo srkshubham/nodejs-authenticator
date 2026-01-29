1b6ebf29392216447f4e22de005d5030b93cde7c
srkshubham <shubhamkumawat149@gmail.com>
2026-01-29 09:45:11 +0530

    added helmet

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(helmet);
+app.use(helmet());
 
 app.get("/health", (_req, res) => {
   res.status(200).send("OK");
